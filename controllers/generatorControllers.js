56502c96d64910fac324549373d80d7b1c6f73a8
DevWithEasy <devwitheasy@gmail.com>
2025-01-27 19:35:35 +0600

    data file add

--- controllers/generatorControllers.js
+++ controllers/generatorControllers.js
@@ -4,10 +4,13 @@ const english = require('../data/en.json')
 const bn_jahirul = require('../data/jahirul_bn.json')
 const bn_mohiuddin = require('../data/muhiuddin_bn.json')
 const suras = require('../data/quran_suras.json')
+const dua_category = require('../data/dua/category.json')
+const dua_names = require('../data/dua/duanames.json')
+const dua_details = require('../data/dua/duadetails.json')
 
 exports.quranCreate = async (req, res, next) => {
   try {
-    const quran  = [] ;
+    const quran = [];
     suras.forEach((s) => {
       const ar = arabic.find(sura => sura.number === s.id)
       const ar_only = arabic_only.find(sura => sura.id === s.id)
@@ -15,7 +18,7 @@ exports.quranCreate = async (req, res, next) => {
       const bnJahirul = bn_jahirul.find(sura => sura.number === s.id)
       const bnMohiuddin = bn_mohiuddin.find(sura => sura.number === s.id)
 
-      const ayahs = ar.ayahs.map((ayah, i)=>{
+      const ayahs = ar.ayahs.map((ayah, i) => {
         return {
           number: ayah.number,
           numberInSurah: ayah.numberInSurah,
@@ -32,9 +35,9 @@ exports.quranCreate = async (req, res, next) => {
         }
       })
 
-      quran.push ({
+      quran.push({
         ...ar,
-        translation : s.translation,
+        translation: s.translation,
         ayahs,
       })
     })
@@ -62,3 +65,54 @@ exports.getQuran = async (req, res, next) => {
     });
   }
 };
+
+exports.duaCreate = async (req, res, next) => {
+  try {
+    //categoty
+    // res.json(dua_category)
+
+    //category dua name
+    // const names = dua_names.filter(name => name.category == '1')
+    // res.json(names)
+
+    const data = []
+
+    dua_category.forEach(category => {
+
+      const names = dua_names.filter(name => name.category == category.id.toString())
+
+      names.forEach(name => {
+        const {dua_global_id,duaname} = name
+        const detail = dua_details.find(d => d.dua_global_id == name.dua_global_id)
+        const {arabic,bottom,reference,top,translations} = detail
+        data.push({
+          id: dua_global_id,
+          category : category.id,
+          name: duaname,
+          arabic,
+          bottom,
+          reference,
+          top,
+          translations,
+        })
+      })
+    })
+    // const names = dua_names.filter(name => name.category == '1')
+
+    // const dua_names = dua_names.filter(name => name.category == '1')
+    // names.forEach(name=>{
+    //   const detail = dua_details.find(d => d.dua_global_id == name.dua_global_id)
+    //   dua.push(detail)
+    // })
+    // res.json(dua)
+
+    res.json(data)
+
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      status: 500,
+      message: error.message,
+    });
+  }
+}
--- routers/generatorRouter.js
+++ routers/generatorRouter.js
@@ -1,10 +1,11 @@
-const { quranCreate, getQuran } = require('../controllers/generatorControllers')
+const { quranCreate, getQuran, duaCreate } = require('../controllers/generatorControllers')
 
 
 const router = require('express').Router()
 
 router.get('/quran',quranCreate)
 router.get('/quran/ar',getQuran)
+router.get('/dua',duaCreate)
 
 
 module.exports = router
